0db2a33f99169089d50a331efb8d88dfb7d382e1
Jesse Shawl <jesse@jshawl.com>
2015-08-17 22:04:58 -0400

    add pushstate

--- public/js/app.js
+++ public/js/app.js
@@ -9,12 +9,7 @@ function supports_history_api() {
 
 function addClicker(link) {
   link.addEventListener("click", function(e) {
-    $.getJSON(e.target.href, function(response){
-      nextUrl.href = response.next
-      prevUrl.href = response.prev
-      dogName.innerHTML = response.current.name
-      imageUrl.src = "/img/" + response.current.src
-    })
+    fetch( e.target.href )
     e.preventDefault()
   }, true);
 }
@@ -29,7 +24,23 @@ window.onload = function() {
   setupHistoryClicks();
   window.setTimeout(function() {
     window.addEventListener("popstate", function(e) {
-      swapPhoto(location.pathname);
+      fetch(location.pathname);
     }, false);
   }, 1);
 }
+
+function fetch(pathname){
+  $.getJSON(pathname, function(response){
+    nextUrl.href = response.next
+    prevUrl.href = response.prev
+    dogName.innerHTML = response.current.name
+    imageUrl.src = "/img/" + response.current.src
+    history.pushState(null, null, response.current.id);
+  })
+}
+
+window.setTimeout(function() {
+  window.addEventListener("popstate", function(e) {
+    swapPhoto(location.pathname);
+  }, false);
+}, 1);
